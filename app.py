fb1e9a43d2d427379d761408ac7e5c72f4d48727
dbenny <deepak_benny@intuit.com>
2022-04-01 13:19:55 +0530

    adding region support

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+import os
 app = Flask(__name__)
+region_name = os.environ['REGION']
+
+# configure default region
+if region_name is None:
+    region_name = "World"
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Universe!'
+    return 'Hello,' + region_name
